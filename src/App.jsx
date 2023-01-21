35aacdc51c9ccdacd608a90bff48b24d295befe8
issam jebrane <issamjebrane65@gmail.com>
2023-01-21 23:40:07 +0100

    adder a timer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 function App() {
   const [quote , setQuote] = useState({})
   const [loading ,setLoading] = useState(true)
+  let [countdown,setCountDown] = useState(10)
   useEffect(()=>{
     const fetchQuotes = async()=>{
       const res = await fetch('https://api.quotable.io/random')
@@ -12,7 +13,15 @@ function App() {
       setQuote(()=>(response))
       setLoading(false)
     }
+    const count = setInterval(() => {
+        setCountDown((prev)=>(
+          prev-1
+        ))
+      
+    }, 1000);
     const interval =  setInterval(() => {
+      setCountDown(10)
+      count
       fetchQuotes()
     }, 10000);
     const time = setTimeout(() => {
@@ -22,6 +31,7 @@ function App() {
     return ()=>{
       clearTimeout(time);
       clearInterval(interval);
+      clearInterval(count);
   }
   },[])
 
@@ -31,7 +41,7 @@ function App() {
       return "clicked"
     })
   }
-
+  console.log(countdown)
   return (
     <div className="App">
       {loading? <h1>loading...</h1>:
@@ -39,6 +49,7 @@ function App() {
          <span>{quote.length}</span>
           <p>{quote.content}</p>
           <h1>{quote.author}</h1>
+          <span>{countdown}s remaning</span>
       </div>}
       </div>
   )
